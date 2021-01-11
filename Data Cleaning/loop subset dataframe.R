dab47114365f8ef5ec9da035bb673775f9656513
sattelmaier <61335664+sattelmaier@users.noreply.github.com>
2021-01-11 16:20:08 +0100

    Update loop subset dataframe.R

--- Data Cleaning/loop subset dataframe.R
+++ Data Cleaning/loop subset dataframe.R
@@ -1,25 +1,27 @@
 library(randomNames)
 
-## dataframe 
+## Create dataframe 
 type = c('B', 'A', 'C', 'D', 'A', 'C', 'B', 'D')
 num = rnorm(mean = 0, sd = 1, n = 8)
 name = randomNames(8)
 
 df <- data.frame(type, num, name, stringsAsFactors=FALSE)
 
-## create factor
-factor <- as.factor(type)
+factor <- as.factor(type) # create factor
 
-# create sub datasets using loop - Bad coding
+## Three Alternatives
+
+### 1) create sub datasets using loop - But bad coding
 for (i in factor){
  assign(paste("group", i, sep = "_"), df[df$type == i, c('num', 'name')])
 }
 
-# Using Split and list2env
+### 2) Using Split and list2env
 df_list <- split(df, df$type) # sorts lists automatically by factor; important for subsetting, but inconvenient 
 df_A = as.data.frame(df_list[1]) 
 
-# One line
-list2env(df_list, envir = .GlobalEnv) #named according to factor
+### 3) One line 
+# 3.1) data frames named according to factor 
+list2env(df_list, envir = .GlobalEnv)
+# 3.2) edit data frame names
 list2env(setNames(df_list, paste0('group', names(df_list))), envir = .GlobalEnv)
-
